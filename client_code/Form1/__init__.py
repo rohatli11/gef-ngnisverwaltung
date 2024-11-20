e824c49dd6dff927fd82b8b1168fc7d09b48512b
rohat.sahbaz@student.htl-rankweil.at <rohat.sahbaz@student.htl-rankweil.at>
2024-11-20 12:14:20 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,14 +13,13 @@ class Form1(Form1Template):
     
     # Any code you write here will run before the form opens.
     self.gefaengnisse_drop_down.items = anvil.server.call('get_gefaengnisse')
-    
     self.label_direktor.text = anvil.server.call('get_direktor') 
     self.label_freie_zellen.text = anvil.server.call('get_zelle') 
     zellennummeranzahl = anvil.server.call('get_zellennummer')
     print(zellennummeranzahl)
     for x in range(len(zellennummeranzahl)):
       print(zellennummeranzahl[x][0])
-      self.repeating_zellen.item = [{'zellennummer': zellennummeranzahl[x][0], 'anzahl_häftlinge': 'TODO'}]
+      self.repeating_zellen.items = [{'zellennummer': zellennummeranzahl[x][0], 'anzahl_häftlinge': 'TODO'}]
 
   def gefaengnisse_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
